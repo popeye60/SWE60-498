6111ae84d5b20c995f053995a658e25be6a9fc41
juthaporn <jutha.1995@gmail.com>
2021-03-12 09:25:34 +0700

    gg

--- src/view/footer.js
+++ src/view/footer.js
@@ -11,7 +11,7 @@ class footer extends React.Component {
                             <div class="col-lg-3 col-md-6 col-sm-6">
                                 <div class="footer__about">
                                     <div class="footer__about__logo">
-                                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                                        <a href="./index.html"><img src="img/logo.png" alt=""/></a>
                                     </div>
                                     <ul>
                                         <li>Address: 60-49 Road 11378 New York</li>
@@ -46,7 +46,7 @@ class footer extends React.Component {
                                     <h6>Join Our Newsletter Now</h6>
                                     <p>Get E-mail updates about our latest shop and special offers.</p>
                                     <form action="#">
-                                        <input type="text" placeholder="Enter your mail">
+                                        <input type="text" placeholder="Enter your mail"/>
                                         <button type="submit" class="site-btn">Subscribe</button>
                                     </form>
                                     <div class="footer__widget__social">
@@ -61,11 +61,11 @@ class footer extends React.Component {
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="footer__copyright">
-                                    <div class="footer__copyright__text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                   <div class="footer__copyright__text"><p> {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
             Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
             {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->*/}
             </p></div> 
-                                    <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
+                                    <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""/></div>
                                 </div>
                             </div>
                         </div>
